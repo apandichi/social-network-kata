51417891f19313a6dceb36908c4be23b8917d819
Alin Pandichi <alin.pandichi@mozaicworks.com>
2017-05-15 11:46:31 +0300

    Remove 'personal' word from test because the backlog does not mention the timeline should be private

--- src/test/groovy/SocialNetworkTests.groovy
+++ src/test/groovy/SocialNetworkTests.groovy
@@ -3,22 +3,22 @@ import spock.lang.Specification
 
 class SocialNetworkTests extends Specification {
 
-	def "Alice has an empty personal timeline"() {
+	def "Alice has an empty timeline"() {
 		given: "A user named Alice"
 		def alice
 
-		when: "Alice inspects her personal timeline"
-		def alicesPersonalTimeline = personalTimelineOf(alice)
+		when: "Alice inspects her timeline"
+		def alicesTimeline = timelineOf(alice)
 
-		then: "Alice sees an empty personal timeline"
-		alicesPersonalTimeline == emptyPersonalTimeline()
+		then: "Alice sees an empty timeline"
+		alicesTimeline == emptyTimeline()
 	}
 
-	def personalTimelineOf(user) {
+	def timelineOf(user) {
 		true
 	}
 
-	def emptyPersonalTimeline() {
+	def emptyTimeline() {
 		true
 	}
 }
